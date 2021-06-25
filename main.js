26aef3e8b24e4a298015f6aabf08068bfcd89444
TongWu2017 <86496287+TongWu2017@users.noreply.github.com>
2021-06-25 14:31:01 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const Discord = require("discord.js")
 const client = new Discord.Client()
 
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
 client.login(require("./config.json").token)
